b225ded89fa24896b8f6df7f32f6e17a20a9e2bd
Magnus <magnus@shipedge.com>
2024-10-12 14:57:14 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   
   await app.listen(envs.port);
 
-  console.log('Hola Mundo - Segundo cambio');
+  console.log('Hola Mundo - Tercer cambio');
 
   logger.log(`Gateway running on port ${envs.port}`)
 }
